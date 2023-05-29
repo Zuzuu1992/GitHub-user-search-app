b9e4193ea34c34a6c51caa36cc7688726cc80464
Ana Izoria <ana.izoria999@gmail.com>
2023-05-29 19:13:59 +0400

    style:work on card markup

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import Address from "../../public/assets/icon-location.svg";
+import Website from "../../public/assets/icon-website.svg";
+import Twitt from "../../public/assets/icon-twitter.svg";
+import WorkPlace from "../../public/assets/icon-company.svg";
+
+import {
+  CardStyle,
+  DesktopAvatar,
+  Detailsbox,
+  UserBio,
+  Avatar,
+  UserDetails,
+  UserNameBox,
+  UserName,
+  Company,
+  JoinDate,
+  Description,
+  CommunityBox,
+  RepoBox,
+  FollowersBox,
+  FollowingBox,
+  Followers,
+  FollowersNumber,
+  Repos,
+  ReposNumber,
+  Following,
+  FollowingNumber,
+  ContactDetails,
+  LocationBox,
+  LocationIcon,
+  Location,
+  BlogBox,
+  BlogIcon,
+  Blog,
+  TwitterBox,
+  TwitterIcon,
+  Twitter,
+  FirmBox,
+  FirmIcon,
+  Firm,
+} from "./Card.styled";
 
 function Card() {
-  return <div>Card</div>;
+  return (
+    <CardStyle>
+      <DesktopAvatar></DesktopAvatar>
+      <Detailsbox>
+        <UserBio>
+          <Avatar></Avatar>
+          <UserDetails>
+            <UserNameBox>
+              <UserName>The Octocat</UserName>
+              <Company>@octocat</Company>
+            </UserNameBox>
+            <JoinDate>joined 25 Ian 2011</JoinDate>
+          </UserDetails>
+        </UserBio>
+        <Description>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+          expedita.
+        </Description>
+        <CommunityBox>
+          <RepoBox>
+            <Repos>Repos</Repos>
+            <ReposNumber>5</ReposNumber>
+          </RepoBox>
+          <FollowersBox>
+            <Followers>Followers</Followers>
+            <FollowersNumber>1542</FollowersNumber>
+          </FollowersBox>
+          <FollowingBox>
+            <Following>Following</Following>
+            <FollowingNumber>155</FollowingNumber>
+          </FollowingBox>
+        </CommunityBox>
+        <ContactDetails>
+          <LocationBox>
+            <LocationIcon src={Address}></LocationIcon>
+            <Location>lorem</Location>
+          </LocationBox>
+          <BlogBox>
+            <BlogIcon src={Website}></BlogIcon>
+            <Blog>lorem ipsum</Blog>
+          </BlogBox>
+          <TwitterBox>
+            <TwitterIcon src={Twitt}></TwitterIcon>
+            <Twitter>kinomano</Twitter>
+          </TwitterBox>
+          <FirmBox>
+            <FirmIcon src={WorkPlace}></FirmIcon>
+            <Firm>giugano</Firm>
+          </FirmBox>
+        </ContactDetails>
+      </Detailsbox>
+    </CardStyle>
+  );
 }
 
 export default Card;
--- src/Components/Card.styled.jsx
+++ src/Components/Card.styled.jsx
@@ -1,1 +1,211 @@
 import styled from "styled-components";
+
+export const CardStyle = styled.div`
+  display: flex;
+  flex-direction:column
+  box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.198567);
+  border-radius: 15px;
+  padding-top: 33px;
+  padding-bottom: 49px;
+  padding-right: 24px;
+  padding-left: 24px;
+  background-color: #fefefe;
+`;
+
+export const DesktopAvatar = styled.img`
+  display: none;
+`;
+
+export const Detailsbox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserBio = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 19.5px;
+`;
+
+export const Avatar = styled.img`
+  height: 70px;
+  width: 70px;
+  /* border-radius: 50%; */
+`;
+
+export const UserDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 6px;
+`;
+
+export const UserNameBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserName = styled.h1`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  color: #2b3442;
+`;
+
+export const Company = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #0079ff;
+`;
+
+export const JoinDate = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #697c9a;
+`;
+
+export const Description = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 25px;
+  color: #4b6a9b;
+  padding-top: 34px;
+  padding-bottom: 23px;
+`;
+
+export const CommunityBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #f6f8ff;
+  border-radius: 10px;
+  padding: 18px 14px;
+`;
+
+export const RepoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+`;
+
+export const Repos = styled.p`
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 16px;
+  text-align: center;
+  color: #4b6a9b;
+`;
+export const ReposNumber = styled.p`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #2b3442;
+`;
+
+export const FollowersBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+`;
+
+export const Followers = styled.p`
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 16px;
+  text-align: center;
+  color: #4b6a9b;
+`;
+export const FollowersNumber = styled.p`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #2b3442;
+`;
+
+export const FollowingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+`;
+
+export const Following = styled.p`
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 16px;
+  text-align: center;
+  color: #4b6a9b;
+`;
+export const FollowingNumber = styled.p`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #2b3442;
+`;
+
+export const ContactDetails = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 17px;
+  margin-top: 24px;
+  justify-items: flex-start;
+`;
+export const LocationBox = styled.div`
+  display: flex;
+  column-gap: 25.25px;
+`;
+export const LocationIcon = styled.img``;
+
+export const Location = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #4b6a9b;
+`;
+
+export const BlogBox = styled.div`
+  display: flex;
+  column-gap: 19.25px;
+`;
+export const BlogIcon = styled.img``;
+
+export const Blog = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #4b6a9b;
+`;
+
+export const TwitterBox = styled.div`
+  display: flex;
+  column-gap: 19.25px;
+`;
+
+export const TwitterIcon = styled.img``;
+
+export const Twitter = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #4b6a9b;
+`;
+
+export const FirmBox = styled.div`
+  display: flex;
+  column-gap: 19.25px;
+`;
+
+export const FirmIcon = styled.img``;
+
+export const Firm = styled.p`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  color: #4b6a9b;
+`;
